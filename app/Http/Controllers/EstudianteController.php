a927e2066338f5515a0f2c58fe5e2e90f0915dc3
Rafael Castillo <RafaelcastilloStark2001@gmail.com>
2025-03-08 15:44:17 -0600

    Se Añadio Controlador EstudianteController con método CRUD

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estudiante;
+use Illuminate\Http\Request;
+
+class EstudianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $estudiantes = Estudiante::all();
+        return view('vista_tabla', compact('estudiantes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('formulario');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'correo' => 'required|email|unique:estudiantes',
+            'fecha_nacimiento' => 'required|date',
+            'ciudad' => 'required|string|max:255',
+        ]);
+
+        Estudiante::create($request->all());
+
+        return redirect()->route('/formulario')->with('success', 'Estudiante agregado.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Estudiante $estudiante)
+    {
+        //
+    }
+}
